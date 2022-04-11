9ddb72b891f649af7e22f4d93c7ce769e3cc645a
Guilherme D. Orsolon <g.orsolon@gmail.com>
2022-04-10 21:23:46 -0300

    Improvements, Domain and Controller Methods

--- extension.js
+++ extension.js
@@ -2,16 +2,19 @@ const vscode = require('vscode');
 const propertiesReader = require('properties-reader');
 const path = require('path');
 
+//Constants of extension
+const optionsOpenDialog = { canSelectFiles: false, canSelectFolders: true};
+const optionsProjectName = { placeHolder: "Project Name", prompt: "Please, input the Project Name (without special characters)"};
+const optionCreateDomainClass = { placeHolder: "Domain Class Name", prompt: "Please, input the Domain Class Name"};
+const optionCreateController = { placeHolder: "Controller Name", prompt: "Please, input the Controller Name (without 'Controller' in their name)."};
+
+//Global vars
+let   application_name = '';
+let   grails_version = '';
+
 async function activate(context) {
 
 	console.log('Your extension "Grails Commands for VSCode" is now active!');
-	//Constants of extension
-	const optionsOpenDialog = { canSelectFiles: false, canSelectFolders: true};
-	const optionsInputBox = { placeHolder: "Project Name", prompt: "Please, input the Project Name (without special characters)"};
-	
-	//Global vars
-	let   application_name = '';
-	let   grails_version = '';
 
 	/** Grails Create App Method*/
 	context.subscriptions.push
@@ -19,34 +22,24 @@ async function activate(context) {
 		vscode.commands.registerCommand
 		('grails-commands-for-vscode.create-app', function()
 			{
-				//Open dialog for select folder to create app
 				vscode.window.showOpenDialog(optionsOpenDialog).then
 				(
 					folders => 
 					{
-						//Check if the folder selected on Open Dialog is not null
 						if (folders != null && folders.length > 0)
 						{
-							//if true, show Input Box for user input the name of Project(Name of Application Grails)
-							vscode.window.showInputBox(optionsInputBox).then
+							vscode.window.showInputBox(optionsProjectName).then
 							(
 								appName => 
 								{
-									//Check if the Name of Project is not null
 									if(appName != null && appName.length > 0)
 									{
-										//if true, create a terminal with initial dir is the path of Open Dialog selected
 										const terminal = vscode.window.createTerminal({cwd: folders[0]});
-										//Set the global variable 'application_name'
 										application_name = appName
-										//show Information Message of execution method
 										let command = `grails create-app ${appName}`;
-										//Send command to terminal and execute it
 										terminal.sendText(command);
-										//Open the folder project on VSCode to work
 										command = `code -r ${appName}`
 										terminal.sendText(command);
-										//Dispose the terminal created
 										terminal.dispose;
 										vscode.window.showInformationMessage(`The application '${appName}' was created.`);
 									}
@@ -65,20 +58,14 @@ async function activate(context) {
 		vscode.commands.registerCommand
 		('grails-commands-for-vscode.run-app', function()
 			{
-				//Resolve the path of properties file
-				let dirPath = path.resolve(vscode.workspace.workspaceFolders[0].uri.fsPath);
-				//Declare reader for properties file in grails project
-				let properties = propertiesReader(path.join(dirPath, 'application.properties'));
-				//Read the Grails Version Propertie
-				grails_version = properties.get("app.grails.version");
-				//Read the Application Name and set global var 'application_name'
-				application_name = properties.get("app.name");
+				//Call function to set grails_version and application_name global variables
+				setApplicationProperties();
 				//Show informations of application.properties file in project
 				vscode.window.showInformationMessage(`Grails Version Project detected: ${grails_version}\nGlobal application_name value: ${application_name}`);
 				//Show information of running application
 				vscode.window.showInformationMessage(`Running App '${application_name}'...`);
 				//Create the terminal with the current project path
-				const terminal = vscode.window.createTerminal(dirPath);
+				const terminal = vscode.window.createTerminal(getWorkspaceDir());
 				//Create and run project on terminal created.
 				let command = `grails run-app ${application_name}`;
 				terminal.sendText(command);
@@ -86,26 +73,64 @@ async function activate(context) {
 		)
 	);
 
+	/** Grails Create Domain Class Method */
 	context.subscriptions.push
 	(
 		vscode.commands.registerCommand
 		('grails-commands-for-vscode.create-domain-class', function()
 			{
-				vscode.window.showInformationMessage('Criando uma Domain Class');
+				vscode.window.showInputBox(optionCreateDomainClass).then
+				{
+					domainName =>
+					{
+						if(domainName != null && domainName.length > 0)
+						{
+							const terminal = vscode.window.createTerminal({cwd: getWorkspaceDir()});
+							let command = `grails create-domain-class ${domainName}`;
+							terminal.sendText(command);
+							terminal.dispose;
+							vscode.window.showInformationMessage(`The Domain Class '${domainName}' was created.`);
+						}
+					}
+				}
 			}
 		)
 	);
 
+  /** Grails Create Controller Method */
 	context.subscriptions.push
 	(
 		vscode.commands.registerCommand
 		('grails-commands-for-vscode.create-controller', function()
 			{
-				vscode.window.showInformationMessage('Criando um controller');
+				vscode.window.showInputBox(optionCreateController).then
+				{
+					controllerName =>
+					{
+						if(controllerName != null && controllerName.length > 0)
+						{
+							const terminal = vscode.window.createTerminal({cwd: getWorkspaceDir()});
+							let command = `grails create-controller ${controllerName}`;
+							terminal.sendText(command);
+							terminal.dispose;
+							vscode.window.showInformationMessage(`The Controller '${controllerName}' was created.`)
+						}
+					}
+				}
 			}
 		)
 	);
 
+}
+
+function setApplicationProperties(){
+	let properties = propertiesReader(path.join(getWorkspaceDir(), 'application.properties'));
+	grails_version = properties.get("app.grails.version");
+	application_name = properties.get("app.name");
+}
+
+function getWorkspaceDir(){
+	return path.resolve(vscode.workspace.workspaceFolders[0].uri.fsPath);
 }
 
 function deactivate() { }

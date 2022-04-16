3cd781161994f293a36517a76b99d90a1158f4eb
Guilherme D. Orsolon <g.orsolon@gmail.com>
2022-04-16 17:32:39 -0300

    Improvements on RunApp Method

--- extension.js
+++ extension.js
@@ -1,39 +1,33 @@
-const vscode = require('vscode');
+const vscode 					 = require('vscode');
 const propertiesReader = require('properties-reader');
-const path = require('path');
+const path 						 = require('path');
+const child						 = require('child_process');
 
 //Constants of extension
-const optionsOpenDialog = { canSelectFiles: false, canSelectFolders: true};
-const optionsProjectName = { placeHolder: "Project Name", prompt: "Please, input the Project Name (without special characters)"};
+const optionsOpenDialog 			= { canSelectFiles: false						, canSelectFolders: true};
+const optionsProjectName 		  = { placeHolder: "Project Name"			, prompt: "Please, input the Project Name (without special characters)"};
 const optionCreateDomainClass = { placeHolder: "Domain Class Name", prompt: "Please, input the Domain Class Name"};
-const optionCreateController = { placeHolder: "Controller Name", prompt: "Please, input the Controller Name (without 'Controller' in their name)."};
+const optionCreateController	= { placeHolder: "Controller Name"	, prompt: "Please, input the Controller Name (without 'Controller' in their name)."};
 
 //Global vars
-let   application_name = '';
-let   grails_version = '';
+let application_name = '';
+let grails_version 	 = '';
 
 async function activate(context) {
 
 	console.log('Your extension "Grails Commands for VSCode" is now active!');
+  
 
 	/** Grails Create App Method*/
-	context.subscriptions.push
-	(
-		vscode.commands.registerCommand
-		('grails-commands-for-vscode.create-app', function()
+	context.subscriptions.push(
+		vscode.commands.registerCommand('grails-commands-for-vscode.create-app',() =>	
 			{
-				vscode.window.showOpenDialog(optionsOpenDialog).then
-				(
-					folders => 
+				vscode.window.showOpenDialog(optionsOpenDialog).then(folders => 
 					{
-						if (folders != null && folders.length > 0)
-						{
-							vscode.window.showInputBox(optionsProjectName).then
-							(
-								appName => 
+						if (folders != null && folders.length > 0){
+							vscode.window.showInputBox(optionsProjectName).then(appName => 
 								{
-									if(appName != null && appName.length > 0)
-									{
+									if(appName != null && appName.length > 0){
 										const terminal = vscode.window.createTerminal({cwd: folders[0]});
 										application_name = appName
 										let command = `grails create-app ${appName}`;
@@ -56,19 +50,16 @@ async function activate(context) {
 	context.subscriptions.push
 	(
 		vscode.commands.registerCommand
-		('grails-commands-for-vscode.run-app', function()
+		('grails-commands-for-vscode.run-app', async function()
 			{
 				//Call function to set grails_version and application_name global variables
 				setApplicationProperties();
 				//Show informations of application.properties file in project
 				vscode.window.showInformationMessage(`Grails Version Project detected: ${grails_version}\nGlobal application_name value: ${application_name}`);
 				//Show information of running application
-				vscode.window.showInformationMessage(`Running App '${application_name}'...`);
-				//Create the terminal with the current project path
-				const terminal = vscode.window.createTerminal(getWorkspaceDir());
-				//Create and run project on terminal created.
-				let command = `grails run-app ${application_name}`;
-				terminal.sendText(command);
+				vscode.window.showInformationMessage(`Running App '${application_name}'...`);	
+				//Call procedure
+				runApp(`grails run-app ${application_name}`);
 			}
 		)
 	);
@@ -131,6 +122,41 @@ function setApplicationProperties(){
 
 function getWorkspaceDir(){
 	return path.resolve(vscode.workspace.workspaceFolders[0].uri.fsPath);
+}
+
+function runApp(command){
+	let linhaTeste = '';
+	let linhaLimpa = '';
+	let promise = new Promise(resolve => {
+		let result = child.exec(command, {cwd: getWorkspaceDir(), name: 'Grails Run-App'});
+
+		result.stdout.on("data", (data) => {
+			linhaTeste = data;
+			linhaLimpa = linhaTeste.match(/(http|https)?:\/\/.+/gi);
+			if(linhaLimpa != null){
+				console.error(linhaLimpa[0]);
+				vscode.env.openExternal(vscode.Uri.parse(linhaLimpa[0]));
+			}
+			resolve(); 
+		});
+
+	});
+
+
+
+	//Call function to set grails_version and application_name global variables
+	/*setApplicationProperties();
+	//Show informations of application.properties file in project
+	vscode.window.showInformationMessage(`Grails Version Project detected: ${grails_version}\nGlobal application_name value: ${application_name}`);
+	//Show information of running application
+	vscode.window.showInformationMessage(`Running App '${application_name}'...`);
+	//Create the terminal with the current project path
+	const terminal = vscode.window.createTerminal({cwd: getWorkspaceDir(), name: 'Grails Run-App'});
+	//Create and run project on terminal created.
+	let command = `grails run-app ${application_name}`;
+	terminal.sendText(command); */
+
+	return promise;
 }
 
 function deactivate() { }
